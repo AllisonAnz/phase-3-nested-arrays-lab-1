86efd259cc8e177b1b73da5478ea5ff6e1812d98
AllisonAnz <AllisonAnzalone@gmail.com>
2021-08-22 13:57:52 -0500

    Completed Assignment

---
+++ Working-With-Nested-Data-Structures.rb
@@ -0,0 +1,32 @@
+# Working With Nested Data Structures 
+# Link https://learning.flatironschool.com/courses/2661/pages/working-with-nested-data-structures?module_item_id=202343
+
+# We are going to collaborate with the computer to discover insights buried in complex nested data structures 
+# Complex nested data structures are things like Arrays, Arrays of Arrays or Hashes of Arrays of Hashes 
+
+# There is a difference between raw data and conclusions drawn from data 
+# When processed correctly, data can teach us many things 
+# The conslusions we extract from data with programming are called insights 
+
+# NDS into Insight 
+#   Raw data in the from of an NDS
+#       Provided from our databases, or 
+#       Provided by a third party 
+# Form hypotheses and write code to provide results from the NDS 
+# Discover insights 
+
+# Insights help us decide where to (or not to) build a warehouse, evaluate whether an investment is likely to be 
+# positive or negative, build alternative major-league baseball staffing strategies, or even the cause of 1854 outbreak 
+
+# Our effors will be divided into two phases 
+# 1. learn to build and evaluate complex NDS so that we can work with them 
+# 2. Identify a process for writing programs that process NDS into insights
+
+# Nested data structures are tools for representing complex information in a way that humans can read and 
+# that computers can process. Reading them and processing them allows us to create insights that improve our lives 
+
+# To start we'll introduce some of the simplest nested data structures 
+# Arrays of Arrays (AoA)
+# Arrays of Hashes (AoH)
+# Hashes of Arrays (HoA)
+# Hashes of Hashes (HoH)
